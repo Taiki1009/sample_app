e1a5dd45e9b7fb5dcd05b9876bfc497ad6a1839b
taiki011609 <s.taiki011609@gmail.com>
2020-09-16 11:37:32 +0900

    User登録成功時のテスト　追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  # 登録失敗時のテスト
   test "invalid signup information" do
     get signup_path
     assert_select 'form[action=?]', signup_path
@@ -18,5 +19,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
+  end
+
+  # 登録成功時のテスト
+  test 'valid signupo information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: {
+        name: "Example User",
+        email: "user@example.com",
+        password: "password",
+        password_confirmation: "password"
+      }}
+    end
+    # App側のコードでは、redirectでshow.erbに移行するので、それについていく
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.blank?
   end
 end
