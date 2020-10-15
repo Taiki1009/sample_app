2ba4aad707a385b29e194e59f917f9463f53683e
taiki011609 <s.taiki011609@gmail.com>
2020-10-15 13:58:11 +0900

    index-page integration-test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
@@ -27,6 +27,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
