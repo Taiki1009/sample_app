c7b8b98e9f1e81aade389e0892d34c04a4e37bd9
taiki011609 <s.taiki011609@gmail.com>
2020-10-27 11:50:30 +0900

    Micropost-View

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,15 @@ User.create!(name: "Example User",
     activated_at: Time.zone.now
     )
 
+    User.create!(name: "NonActivate User",
+        email:         "nonactivate@railstutorial.org",
+        password:              "foobar",
+        password_confirmation: "foobar",
+        admin: true,
+        activated: false,
+        activated_at: Time.zone.now
+        )
+
 99.times do |n|
     name = Faker::Name.name
     email = "example-#{n+1}@railstutorial.org"
@@ -26,4 +35,10 @@ User.create!(name: "Example User",
         activated: true,
         activated_at: Time.zone.now
         )
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+    content = Faker::Lorem.sentence(5)
+    users.each { |user| user.microposts.create!(content: content) }
 end
