e3653ae049c0ef2825ad596c3d0a0145ea4222d0
taiki011609 <s.taiki011609@gmail.com>
2020-11-16 17:38:14 +0900

    micropostView & Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,14 @@ User.create!(name: "Example User",
     activated_at: Time.zone.now
     )
 
-    User.create!(name: "NonActivate User",
-        email:         "nonactivate@railstutorial.org",
-        password:              "foobar",
-        password_confirmation: "foobar",
-        admin: true,
-        activated: false,
-        activated_at: Time.zone.now
-        )
+User.create!(name: "NonActivate User",
+    email:         "nonactivate@railstutorial.org",
+    password:              "foobar",
+    password_confirmation: "foobar",
+    admin: true,
+    activated: false,
+    activated_at: Time.zone.now
+    )
 
 99.times do |n|
     name = Faker::Name.name
@@ -37,8 +37,17 @@ User.create!(name: "Example User",
         )
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
     content = Faker::Lorem.sentence(5)
     users.each { |user| user.microposts.create!(content: content) }
 end
+
+# リレーションシップ
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
